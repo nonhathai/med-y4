53a7191b70df09d199f545bc209c05e6f938434c
nonhathai <nonhathai.bud@cpird.in.th>
2026-04-02 16:19:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // 🌐 URL ของ Google Apps Script (ตัวใหม่ที่รวมโค้ดแล้ว)
     // สามารถใช้ลิงก์เดียวกันได้เลย เพราะระบบ Backend แยกส่วนให้แล้ว
-    WEB_APP_API: "https://script.google.com/macros/s/AKfycbzMByl1n4eIt9Mz5z2-nP-oVWx07F1azJIdjZYJ2vALHCPkD76SWl17A1-VqU4-0Hcm/exec",
+    WEB_APP_API: "https://script.google.com/macros/s/AKfycbwlrtncLpPm3sS7DkJr-Qp8RVXv873dj2zKzIZ5715jCiX7c-blO1jKSJ2f3Fi493V8lQ/exec",
 
     // 📱 LIFF IDs
     LIFF_ID_CHECKIN: "2009669864-RS4IQsdL",
